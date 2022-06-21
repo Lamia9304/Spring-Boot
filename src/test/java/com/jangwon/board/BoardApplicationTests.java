e88e25ca09081e33b840c907fbf98ad88249beca
Lamia9304 <lamia9304@naver.com>
2022-06-22 00:31:17 +0900

    Spring boot 게시판 초기설정

---
+++ src/test/java/com/jangwon/board/BoardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jangwon.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardApplicationTests {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jangwon/board/Controller/BoardController.java
@@ -0,0 +1,12 @@
+package com.jangwon.board.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoardController {
+    @GetMapping("/test")
+    public String test() {
+        return "Hello World!";
+    }
+}
